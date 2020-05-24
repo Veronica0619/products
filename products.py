b4649d322e1080eb5b7547451d80c73525cec8ae
Veronica <veronicavv0619@gmail.com>
2020-05-24 16:08:33 +0800

    header commit

--- products.py
+++ products.py
@@ -19,7 +19,8 @@ for p in products:
 #'abc'+'123'='abc123'
 #'abc'*3 = 'abcabcabc'
 
-with open('products.csv', 'w') as f: # 'w' 是寫入模式 # as 當作
+with open('products.csv', 'w', encoding='utf-8') as f: # 'w' 是寫入模式 # as 當作
+	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n') #f.wtite 才能真正寫入
-
+#encoding='utf-8' 最廣泛使用的編碼
