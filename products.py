9866d2b72b6a9f173fa4df09cf2eebe1bb2d8935
Veronica <veronicavv0619@gmail.com>
2020-05-24 16:59:49 +0800

    add check file  commit

--- products.py
+++ products.py
@@ -1,17 +1,27 @@
- #讀取檔案
-products = []
-with open ('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue #繼續，跳過直接到下一回
-		name, price = line.strip().split(',') #先把尾巴換行符號 /n 去掉 #split 切割 用, 當切割的標準
+import os #operating system
+
+#讀取檔案
+products = [] #放在最外面，不管檔案存不存在都要用
+if os.path.isfile('products.csv'): # 作業系統裡path 模組的isfile功能，檢查檔案在不在
+	print('Yeah! 找到檔案了!')
+	with open ('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue #繼續，跳過直接到下一回
+			name, price = line.strip().split(',') #先把尾巴換行符號 /n 去掉 #split 切割 用, 當切割的標準
 		#name = s[0]
 		#price = s[1]
-		products.append([name , price])
-print(products)
+			products.append([name , price])
+	print(products)
+else:
+	print('找不到檔案....')
+
+ #讀取檔案
+
+
 
 # 2 維度清單，讓使用者輸入
-products = []
+
 while True:
 	name = input('請輸入商品名稱：')
 	if name == 'q':
